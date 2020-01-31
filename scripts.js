1b9bd8518eae4bb4f034eb7028b9d57b821b591e
IB_Student <student@InnovateBhams-MacBook-Pro.local>
2020-01-31 16:22:04 -0600

    third commit

--- scripts.js
+++ scripts.js
@@ -1,15 +1,18 @@
 let sentences = [
   'ten ate neite ate nee enet ite ate inet ent eate',
- 'Too ato too nOt enot one totA not anot tOO aNot', 
- 'oat itain oat tain nate eate tea anne inant nean',
+  'Too ato too nOt enot one totA not anot tOO aNot',
+  'oat itain oat tain nate eate tea anne inant nean',
   'itant eate anot eat nato inate eat anot tain eat',
-   'nee ene ate ite tent tiet ent ine ene ete ene ate'
-  ];
+  'nee ene ate ite tent tiet ent ine ene ete ene ate'
+];
 
-  let sentIndex = 0;
-  let letterIndex = 0;
-  let currentSentence = sentences[sentIndex];
-  let currentLetter = currentSentence[letterIndex];
+let sentIndex = 0;
+let letterIndex = 0;
+let currentSentence = sentences[sentIndex];
+let currentLetter = currentSentence[letterIndex];
+let start;
+let finish;
+let errors = 0;
 
 $("#sentence").text(currentSentence);
 $("#target-letter").text(currentLetter);
@@ -34,31 +37,51 @@ $(document).keypress(function (e) {
   console.log(letterIndex);
   console.log(currentSentence.length)
   $('#' + e.keyCode).addClass('highlight');
-  if(currentSentence.charCodeAt(letterIndex) === e.keyCode) {
+  if (currentSentence.charCodeAt(letterIndex) === e.keyCode) {
     $('#feedback').append('<span class="glyphicon glyphicon-ok"></span>');
     letterIndex++;
-  
-  $('#yellow-block').css('left', '+=17.5px');
-  
 
- 
- let currentLetter = currentSentence[letterIndex];
- $("#target-letter").text(currentLetter);
-} else {
+    $('#yellow-block').css('left', '+=17.5px');
+
+
+
+    let currentLetter = currentSentence[letterIndex];
+    $("#target-letter").text(currentLetter);
+  } else {
     $('#feedback').append('<span class="glyphicon glyphicon-remove"></span>')
-    alert('INCORRECT');
 
-}
-
+  }
 
 
-if (letterIndex == currentSentence.length) {
-  letterIndex = 0
-  sentIndex++
-  console.log(sentIndex);
-  $('#sentence').text(sentences[sentIndex]);
-  $('#feedback').text('');
-}
 
+  if (letterIndex == currentSentence.length) {
+    letterIndex = 0
+    sentIndex++
+    console.log(sentIndex);
+    $('#sentence').text(sentences[sentIndex]);
+    currentSentence = sentences[sentIndex];
+    currentLetter = currentSentence[letterIndex];
+    $('#feedback').text('');
+    $('#yellow-block').css('left', '17.5px');
+    $('#target-letter').text(currentLetter);
+
+  }
+  // let numberOfWords = 54;
+  // numberOfWords / minutes -2 * NumberofMistakes
+  if (sentIndex > sentences.length - 1) {
+    finish = event.timeStamp;
+    let time = (finish - start);
+    time /= 600000;
+    let speed = Math.round((54 / time) - (errors * 2));
+    $("#next-letter").text("Your score is " + speed + " words per minute");
+    setTimeout(function () {
+      let tryAgain = confirm("Would you like to try again?");
+      if (tryAgain == true) {
+        window.location.reload();
+      } else {
+        return;
+      };
+    }, 4000);
+  }
 
 })
